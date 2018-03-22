cbb65b147bb193d8fe86071bb4e8596690ca21cc
Nils <nilsbergmann@noim.io>
2018-03-22 16:21:01 +0100

    [WIP] Need help with align-content-*

--- examples/grid/demo.vue
+++ examples/grid/demo.vue
@@ -3,9 +3,9 @@
     <v-flex xs12>
       <div class="resultContainer">
         <v-layout v-bind="layoutAttributes">
-          <div class="item elevation-5"></div>
-          <div class="item elevation-5"></div>
-          <div class="item elevation-5"></div>
+          <div class="item elevation-5"/>
+          <div class="item elevation-5"/>
+          <div class="item elevation-5"/>
         </v-layout>
       </div>
     </v-flex>
@@ -19,7 +19,7 @@
                   v-for="n in alignmentsAvailable"
                   :key="n"
                   :label="n === '' ? 'Nothing' : n"
-                  :value="n"/>
+                  :value="n"></v-radio>
               </v-radio-group>
             </v-flex>
             <v-flex xs12 md4>
@@ -28,7 +28,7 @@
                   v-for="n in justifyAvailable"
                   :key="n"
                   :label="n === '' ? 'Nothing' : n"
-                  :value="n"/>
+                  :value="n"></v-radio>
               </v-radio-group>
             </v-flex>
             <v-flex xs12 md4>
@@ -37,18 +37,32 @@
                   <v-checkbox
                     label="reverse"
                     v-model="reverse"
-                    hide-details/>
+                    hide-details></v-checkbox>
                   <v-checkbox
                     label="fill-height"
                     v-model="fillHeight"
-                    hide-details/>
+                    hide-details></v-checkbox>
                   <v-radio
                     v-for="n in flexDirectionAvailable"
                     :key="n"
                     :label="n"
-                    :value="n"/>
+                    :value="n"></v-radio>
                 </v-radio-group>
               </v-layout>
+            </v-flex>
+          </v-layout>
+        </div>
+        <!-- I have no plan what the align-content-* attributes are doing. Need help. -->
+        <div v-if="false">
+          <v-layout row>
+            <v-flex xs12 md4>
+              <v-radio-group v-model="alignmentContent">
+                <v-radio
+                  v-for="n in alignmentsContentAvailable"
+                  :key="n"
+                  :label="n === '' ? 'Nothing' : n"
+                  :value="n"></v-radio>
+              </v-radio-group>
             </v-flex>
           </v-layout>
         </div>
@@ -56,44 +70,47 @@
     </v-flex>
     <v-flex xs12>
       <h5>Output:</h5>
-      <code>{{formatAttributes(layoutAttributes)}}</code>
+      <code>{{ formatAttributes(layoutAttributes) }}</code>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
   export default {
-    data() {
+    data () {
       return {
         alignmentsAvailable: ['align-center', 'align-end', 'align-space-around', 'align-space-between', 'align-start', ''],
         alignment: 'align-center',
+        alignmentsContentAvailable: ['align-content-center', 'align-content-end', 'align-content-space-around', 'align-content-space-between', 'align-content-start', ''],
+        alignmentContent: 'align-content-center',
         justifyAvailable: ['justify-center', 'justify-end', 'justify-space-around', 'justify-space-between', 'justify-start', ''],
         justify: 'justify-center',
         reverse: false,
         flexDirectionAvailable: ['row', 'column'],
         flexDirection: 'row',
         fillHeight: true
-      };
+      }
     },
     computed: {
-      layoutAttributes() {
+      layoutAttributes () {
         return {
           [this.alignment]: true,
           [this.justify]: true,
           [this.flexDirection]: true,
+          [this.alignmentContent]: true,
           reverse: this.reverse,
-          ['fill-height']: this.fillHeight
-        };
+          'fill-height': this.fillHeight
+        }
       }
     },
     methods: {
-      formatAttributes(attributes) {
-        const attributeArray = [];
+      formatAttributes (attributes) {
+        const attributeArray = []
         for (const Key in attributes) {
-          if (!attributes.hasOwnProperty(Key) || Key === '' || attributes[Key] === false) continue;
-          attributeArray.push(Key.trim());
+          if (!attributes.hasOwnProperty(Key) || Key === '' || attributes[Key] === false) continue
+          attributeArray.push(Key.trim())
         }
-        return `<v-layout ${attributeArray.join(' ')}/>`;
+        return `<v-layout ${attributeArray.join(' ')}/>`
       }
     }
 
